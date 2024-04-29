19df63262218deb24009d842b000dff3b22053b2
umair-rao <umairmuqeem@gmail.com>
2024-04-29 14:48:43 +0500

    Project Setup

---
+++ app/components/PagesCard.js
@@ -0,0 +1,10 @@
+
+const PagesCard = () => {
+  return (
+    <div>
+      <h1>Muqeem</h1>
+    </div>
+  )
+}
+
+export default PagesCard
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import PagesCard from "./components/PagesCard";
 
 
 export default function Home() {
   return (
     <main>
       <h1>Umair</h1>
+      <PagesCard/>
     </main>
   );
 }
